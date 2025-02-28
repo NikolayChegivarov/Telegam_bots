545095fcc49835ef56d148f2761fc4a1f8470787
Nikolai Gusarov <nikolai_polos@mail.ru>
2025-02-28 13:36:10 +0300

    create database

--- Telegrambot_Freelance/create_database.py
+++ Telegrambot_Freelance/create_database.py
@@ -4,68 +4,61 @@ from psycopg2 import sql
 import os
 
 load_dotenv()
-# Загрузка переменных окружения
-TELEGRAM_TOKEN_BOT = os.getenv('TELEGRAM_TOKEN_BOT')
-HOST = os.getenv('HOST')
-USER = os.getenv('USER')
-PASSWORD_DB = os.getenv('PASSWORD_DB')
-PORT = os.getenv('PORT')
-NAME_DB = os.getenv('NAME_DB', 'Telegrambot_Freelance')
-print(PASSWORD_DB)
-print(USER)
+
+
+def connect_to_database(dbname=None):
+    """Функция устанавливает подключение к базе данных PostgreSQL."""
+    try:
+        connection = psycopg2.connect(
+            host=os.getenv("HOST"),
+            database=dbname if dbname else os.getenv("NAME_DB"),
+            user=os.getenv("USER"),
+            password=os.getenv("PASSWORD_DB"),
+            port=os.getenv("PORT")
+        )
+        # print(f"Подключение к PostgreSQL успешно установлено: {connection}")
+        return connection
+    except (Exception, psycopg2.Error) as error:
+        print(f"Ошибка при подключении к PostgreSQL: {error}")
+        return None
 
 
 def check_db_connection():
-    """Проверяем есть ли подключение к postgres."""
-    try:
-        # Попытка подключения к стандартной базе данных.
-        conn = psycopg2.connect(
-            host=HOST,
-            user=USER,
-            password=PASSWORD_DB,
-            port=PORT,
-            dbname="postgres"  # Стандартная бд postgres.
-        )
-        conn.close()
-        print("Соединение с PostgreSQL успешно установлено.")
+    """Проверка соединения с PostgreSQL."""
+    connection = connect_to_database(dbname="postgres")  # Подключение к стандартной базе данных
+    if connection:
+        connection.close()
+        print("Соединение с PostgreSQL успешно проверено.")
         return True
-    except Exception as e:
-        print(f"Ошибка при подключении к PostgreSQL: {e}")
-        return False
+    return False
 
 
 def check_and_create_db():
-    """Если есть подключение проверяем наличие нашей бд.
-    Если бд нет, создаем ее.
-    """
+    """Проверка наличия базы данных и её создание, если она отсутствует."""
+    connection = connect_to_database(dbname="postgres")  # Подключение к стандартной базе данных
+    if not connection:
+        return
 
     try:
-        # Подключение к стандартной базе данных для проверки наличия целевой базы данных.
-        conn = psycopg2.connect(
-            host=HOST,
-            user=USER,
-            password=PASSWORD_DB,
-            port=PORT,
-            dbname="postgres"
-        )
-        conn.autocommit = True
-        cursor = conn.cursor()
+        connection.autocommit = True
+        cursor = connection.cursor()
 
         # Проверка наличия базы данных
-        cursor.execute(sql.SQL("SELECT 1 FROM pg_database WHERE datname = %s"), [NAME_DB])
+        cursor.execute(sql.SQL("SELECT 1 FROM pg_database WHERE datname = %s"), [os.getenv("NAME_DB")])
         exists = cursor.fetchone()
 
         if not exists:
             # Создание базы данных, если она отсутствует
-            cursor.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(NAME_DB)))
-            print(f"База данных {NAME_DB} успешно создана.")
+            cursor.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(os.getenv("NAME_DB"))))
+            print(f"База данных {os.getenv('NAME_DB')} успешно создана.")
         else:
-            print(f"База данных {NAME_DB} уже существует.")
+            print(f"База данных {os.getenv('NAME_DB')} уже существует.")
 
         cursor.close()
-        conn.close()
     except Exception as e:
         print(f"Ошибка при проверке или создании базы данных: {e}")
+    finally:
+        connection.close()
 
 
 if __name__ == "__main__":
